124462650995adda1e983d8a5d1bad3a0a00f6f1
michaelf <michaelferretti.new@gmail.com>
2018-03-27 10:24:59 -0400

    Add Vue js and minified files for production

---
+++ public/index.js
@@ -0,0 +1,14 @@
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
