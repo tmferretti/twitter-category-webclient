39a261792d89db4537841037040afe52f74538e0
Michael <michael.ferretti@outlook.com>
2018-03-29 03:12:25 -0400

    Fix changeTab function

--- public/index.js
+++ public/index.js
@@ -12,8 +12,7 @@ var HomePage = {
   created: function() {
   	axios.get('https://hidden-everglades-54760.herokuapp.com/v1/categories', { crossdomain: true }).then(function(response) {
       this.categories = response.data
-      this.activeCategory = this.categories[0]
-      // console.log(this.categories)
+
       for (var i = 0; this.categories[i]; i++) {
         this.tweets.push(this.categories[i].tweets); 
       };
@@ -23,39 +22,29 @@ var HomePage = {
     }.bind(this));
   },
   methods: {
+    
     changeActive: function(category, categories, tweets) {
-    //   
-      console.log(category)
-
+      console.log(category.tweets.length)
       for (var i = 0; tweets[i]; i++) {
         tweets[i].isActive = false;
       };
 
       for (var i = 0; category.tweets[i]; i++) {
-        // console.log(category.tweets[i].isActive)
-        if (category.tweets[i].isActive != true) {
-          // console.log(category.tweets[i].isActive);    
+        if (category.tweets[i].isActive != true) {   
           category.tweets[i].isActive = true;
         };
       };
 
       if (category.isActive != true) {
         console.log("category.isActive is not true")
-
         for (var i = 0; categories[i]; i++) {
           categories[i].isActive = false;
         };
-
         category.isActive = !category.isActive;
-
-        // for (var i = 0; category.tweets[i]; i++) {
-          // category.tweets[i].isActive = !category.tweets[i].isActive;
-        // };
-        
-      
       };
     }.bind(this)
   },
+
   computed: {}
 };
 
